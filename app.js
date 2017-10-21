1dd392f327aeb4d5a8614936cdb1f1fd95575300
Bakhumhlea <Tatuu@bakhumhlea.com>
2017-10-21 04:08:05 -0700

    Assignment-7-to-do-app-complete

--- app.js
+++ app.js
@@ -5,20 +5,28 @@ function onReady() {
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-    
+
     let title = newToDoText.value;
     let newLi = document.createElement('li');
     let checkbox = document.createElement('input');
+    let deleteButton = document.createElement('button');
     checkbox.type = "checkbox";
+    deleteButton.type = "reset";
 
     newLi.textContent = title;
     newLi.appendChild(checkbox);
-
+    newLi.appendChild(deleteButton);
+    deleteButton.textContent = "Remove";
+    deleteButton.addEventListener('click', removeList, false);
     toDoList.appendChild(newLi);
 
     newToDoText.value = '';
   });
 }
+function removeList() {
+  this.parentNode.parentNode.removeChild(this.parentNode);
+}
+
 
 window.onload = function() {
    onReady();
